ccb3a4081df4657ab420a373425d38b9df839f2b
Mohammed Abu Shmala <mhmdjaber120161209@gmail.com>
2022-02-22 16:25:57 +0200

    Add project files.

---
+++ ASP.NETCoreRDLC2/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreRDLC2
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ ASP.NETCoreRDLC2/Controllers/Report.cs
@@ -0,0 +1,52 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreRDLC2.Controllers
+{
+    public class Report : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvirnoment;
+        private readonly ApplicationDbContext _context;
+
+
+        public Report(IWebHostEnvironment webHostEnvirnoment, ApplicationDbContext context)
+        {
+            this._webHostEnvirnoment = webHostEnvirnoment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Print()
+        {
+            string mimtype = "";
+            int extension = 1;
+            var path = $"{ this._webHostEnvirnoment.WebRootPath}\\Reports\\Report2.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            //parameters.Add("rp1", "Welcome to FoxLearn");
+            var products = await GetProducts();
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("DataSet1", products);
+                
+
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimtype);
+
+            return File(result.MainStream, "application/pdf");
+        }
+
+        public async Task<IEnumerable<Post>> GetProducts()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+    }
+}
